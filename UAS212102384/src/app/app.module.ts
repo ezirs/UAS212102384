6bd36978f08be7e0b5b4d72e6672f046d8f5d49e
SeptiWahyudi90 <septiwahyudi90@gmail.com>
2024-01-25 11:50:16 +0700

    Fisrt Commit

--- UAS212102384/src/app/app.module.ts
+++ UAS212102384/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, MahasiswaComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [HttpClient],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- UAS212102384/src/app/mahasiswa/mahasiswa.component.ts
+++ UAS212102384/src/app/mahasiswa/mahasiswa.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 declare const $: any;
 
